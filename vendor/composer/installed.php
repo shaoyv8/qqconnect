5ececeabeb55b30b250343d03e6898098101000a
shaoyv88 <slf5520@126.com>
2022-09-05 15:36:36 +0800

    1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'shaoyv8/qqconnect',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '97dc46a9efcc6982a5de429f53db8c672d78dbc7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'shaoyv8/qqconnect' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '97dc46a9efcc6982a5de429f53db8c672d78dbc7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
